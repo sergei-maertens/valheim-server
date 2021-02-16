e6120563935eb75933c4c7fe0dbecf5223f44ae9
Sergei Maertens <sergei@maykinmedia.nl>
2021-02-16 21:46:39 +0100

    idk

--- start_server.sh
+++ start_server.sh
@@ -9,10 +9,10 @@ export SteamAppId=892970
 ./valheim_server.x86_64 \
     -nographics \
     -batchmode \
-    -name $SERVER_NAME \
+    -name "$SERVER_NAME" \
     -port 2456 \
-    -world $SERVER_WORLD \
-    -password $SERVER_PASSWORD \
+    -world "$SERVER_WORLD" \
+    -password "$SERVER_PASSWORD" \
     -public 1
 
 export LD_LIBRARY_PATH=$templdpath
